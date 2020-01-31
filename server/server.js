e4182ab6a0e291827166f2c7ca35edd83d4d5896
Kostya Demidchik <k.demidchik@tapston.com>
2020-01-31 20:42:21 +0500

    JS-3/Server-fix

--- server/server.js
+++ server/server.js
@@ -11,8 +11,6 @@ app.use(cors({
 
 // генерация html на локальном сервере по адресу http://localhost:8080/task1.html
 app.use('/', express.static(process.cwd() + '/src/task1'));
-// генерация html на локальном сервере по адресу http://localhost:8080/task2.html
-app.use('/', express.static(process.cwd() + '/src/task2'));
 
 // для task3 первый запрос с искуственной задержкой для тестирования
 app.get('/first', (req, res, next) => {
